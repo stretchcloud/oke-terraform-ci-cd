197f791ca818fd409191b670c9fcca4e03217c50
stretchcloud <jit2600@gmail.com>
2020-09-02 15:38:20 +0100

    Update state-ostore.tf

--- state-ostore.tf
+++ state-ostore.tf
@@ -1,6 +1,6 @@
 terraform {
    backend "http" {
-     address = "https://objectstorage.us-phoenix-1.oraclecloud.com/p/lsH5fDi9X5vQZxgO7ubemT7NnUw4G1_MVEpWBuinqsc/n/intprasenjits/b/oke-bucket/o/terraform.tfstate" 
+     address = "<PAR-URL>" 
      update_method = "PUT" 
    }
 }
